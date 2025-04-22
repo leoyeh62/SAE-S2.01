03540127abd215fa63d7b2188f039aff1a83713d
leoyeh62 <morel.leo23@gmail.com>
2025-04-22 13:52:31 +0200

    Update MethodePaiement.java

--- SAE_S201/src/SAET/MethodePaiement.java
+++ SAE_S201/src/SAET/MethodePaiement.java
@@ -1,5 +1,5 @@
 package SAET;
-
+// définition de classe abstraite MethodePaiement
 public abstract class MethodePaiement {
     public abstract void payer(double montant);
     public abstract void rembourser(double montant);
