969e666ce5a6cdf94439d77514ec7471e0fd030d
Gamekiii <gam3ki@gmail.com>
2025-04-22 09:50:48 +0200

    StockEau.java

--- SAE_S201/src/StockEau.java
+++ SAE_S201/src/StockEau.java
@@ -1,7 +1,27 @@
 package SAET;
 
 public class StockEau {
+    private Eau eau;
+    private Entrepot entrepot;
+    private int quantite;
 
+    public StockEau(Eau eau, Entrepot entrepot, int quantite) {
+        this.eau = eau;
+        this.entrepot = entrepot;
+        this.quantite = quantite;
+    }
+
+    public Eau getEau() {
+        return eau;
+    }
+
+    public Entrepot getEntrepot() {
+        return entrepot;
+    }
+
+    public int getQuantite() {
+        return quantite;
+    }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
