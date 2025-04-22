8aaef806cb0830fe9b337ddae32a84eadb14ea7b
leoyeh62 <morel.leo23@gmail.com>
2025-04-22 13:45:04 +0200

    Update ClientEntreprise.java

--- SAE_S201/src/SAET/ClientEntreprise.java
+++ SAE_S201/src/SAET/ClientEntreprise.java
@@ -3,18 +3,18 @@ package SAET;
 public class ClientEntreprise extends Client {
 	private String nom;
 	private int SIRET;
-		
+    //Constructeur		
     public ClientEntreprise(int code, int dateInscription, Adresse adresse,String telephone, String mail, int pointsFidelite,String nom,int SIRET) {
         super(code, dateInscription, adresse, telephone, mail, pointsFidelite);
         this.nom = nom;
         this.SIRET = SIRET;
     }
-    
+    //méthode d'ajout de points de fidélités
     public void ajoutPointsFidelite(double achat) {
         double points = (achat / 1000) * 10;
         this.pointsFidelite += points;
     }
-
+    //getters	
     public String getTypeClient() {
         return "Entreprise";
     }
@@ -26,7 +26,7 @@ public class ClientEntreprise extends Client {
     public int GetSIRET() {
     	return SIRET;
     }
-    
+    //setters
     public void SetNom(String nom) {
     	this.nom=nom;
     }
@@ -34,7 +34,7 @@ public class ClientEntreprise extends Client {
     public void SetSIRET(int SIRET) {
     	this.SIRET=SIRET;
     }
-
+        //méthode toString
 	@Override
 	public String toString() {
 		return "ClientEntreprise [nom=" + nom + ", SIRET=" + SIRET + ", getCode()=" + getCode()
