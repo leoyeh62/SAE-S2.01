ddde6cbef8339cb8a56665a0a3b81b0311cdd295
modulvampir <imafashionkilla777@gmail.com>
2025-04-22 09:44:46 +0200

    Add files via upload

--- SAE_S201/src/entrepot.java
+++ SAE_S201/src/entrepot.java
@@ -1,5 +1,14 @@
 package Entrepot;
 
-public class entrepot {
+public class entrepot extends O_de_france {
+	private String nom;
+	private String adresse;
+	private int code; 
+	
+	public entrepot (String adresse, String nom, int code) {
+		this.adresse = adresse;
+		this.nom = nom;
+		this.code = code;
+	}
 
 }
