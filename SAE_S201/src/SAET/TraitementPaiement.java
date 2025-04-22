043aa4abe2f66dda7530011d2f49852c44201053
leoyeh62 <morel.leo23@gmail.com>
2025-04-22 13:58:21 +0200

    Update TraitementPaiement.java

--- SAE_S201/src/SAET/TraitementPaiement.java
+++ SAE_S201/src/SAET/TraitementPaiement.java
@@ -1,5 +1,5 @@
 package SAET;
-
+//déclaration de l'interface TraitementPaiement
 public interface TraitementPaiement {
 	void executePaiement(Paiement paiement);
 	void remboursement(Paiement paiement);
