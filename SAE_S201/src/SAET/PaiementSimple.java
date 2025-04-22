c9aacc229ea096f97c6ed1ce9365e30212ecd5bd
leoyeh62 <morel.leo23@gmail.com>
2025-04-22 13:56:00 +0200

    Update PaiementSimple.java

--- SAE_S201/src/SAET/PaiementSimple.java
+++ SAE_S201/src/SAET/PaiementSimple.java
@@ -2,11 +2,19 @@ package SAET;
 
 
 public class PaiementSimple implements TraitementPaiement {
-
+    /**
+     * Exécute un paiement en utilisant la méthode de paiement spécifiée dans l'objet Paiement.
+     *
+     * @param paiement l'objet contenant les informations nécessaires pour payer (montant et méthode de paiement)
+     */
     public void executePaiement(Paiement paiement) {
         paiement.getMethode().payer(paiement.getMontant());
     }
-
+    /**
+     * Effectue un remboursement en utilisant la méthode de paiement spécifiée dans l'objet Paiement.
+     *
+     * @param paiement l'objet contenant les informations nécessaires pour rembourser (montant et méthode de paiement)
+     */
     public void remboursement(Paiement paiement) {
         paiement.getMethode().rembourser(paiement.getMontant());
     }
